19f8a935cc6b2b127505cd24d38d2fd740446f39
amandamheredia <amanda.mnzh@gmail.com>
2022-04-13 12:46:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import './mesas.js';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import Mesas from './mesas.js';
+
+
+export default function App() {
+  return (
+    <div className="menu-container">
+      <header className="menu-header">
+        <p>
+          ¡Bienvenida al menu de Burger Queen!
+        </p>
+
+         <button className= "mesas-button">Mesas</button>
+         <button className= "cocina-button">Cocina</button>
+         
+        {/* <Link to="mesas" className='mesas'>Mesas</Link>
+        <Link to="cocina" className='cocina'>Cocina</Link> */}
+
+        <p>Elige el modo que necesitas</p>
+        {/* <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route path="mesas" element={<Mesas />} />
+        </Route>
+      </Routes> */}
+      </header>
+    </div>
+  );
+}
+
+// function Layout() {
+//   return (
+//     <div>
+//       {/* A "layout route" is a good place to put markup you want to
+//           share across all the pages on your site, like navigation. */}
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="mesas">Mesas</Link>
+//           </li>
+//           <li>
+//             <Link to="/cocina">Cocina</Link>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <hr />
+
+//       {/* An <Outlet> renders whatever child route is currently active,
+//           so you can think about this <Outlet> as a placeholder for
+//           the child routes we defined above. */}
+//       <Outlet />
+//     </div>
+//   );
+// }
+
+
---
+++ src/mesas.js
@@ -0,0 +1,14 @@
+export default function Mesas() {
+    return (
+      <div className="mesas-container">
+        <header className="mesas-header">
+          <p>
+            Mesas
+          </p>
+  
+          <p>Menú desayuno</p>
+          <p>Menú almuerzo y cena</p>
+        </header>
+      </div>
+    );
+  }
