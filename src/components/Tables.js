67fef5bb5a76cdb11cac349ced6bacf763ada119
amandamheredia <amanda.mnzh@gmail.com>
2022-05-03 11:58:23 -0400

    suma total funcionando

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -8,16 +8,12 @@ export default function Tables () {
 
   //ESTADO DE LA COMIDA
   const [comida, setComida] = useState(menu.menu)
-  //ESTADO DEL TOTAL
-  // const [total, setTotal] = useState(() => totalInicial())
   //ESTADO DEL CARRITO
   const [cart, setCart] = useState ([])
-  
   
   useEffect (() =>{
    fetch("menu.json")
    .then((response) => {
-    //  console.log(response.json)
      return response.json()
    })
    .then(() => {
@@ -26,34 +22,24 @@ export default function Tables () {
   })
   }, [])
 
-  // function totalInicial () {
-  //   console.log ('hola lesbianas de chile')
-  
-  //   return (
-  //     0
-  //   )
-  //   }
-
-  // function borrarItem () {
-
-  //   setTotal(prevItem => prevItem - 1)  
-  // }
-
-  // function agregarItem () {
-   
-  //   setTotal(prevItem => prevItem + 1) 
-    
-  // }
-
+  //FUNCION PARA AGREGAR ITEMS AL CARRITO
   const addToCart = (item) => {
     console.log("estamos en el carrito")
     setCart([...cart, {...item}])
   }
-
+  //FUNCION PARA ELIMINAR ITEMS DEL CARRITO
   const removeFromCart = (productToRemove) => {
     setCart(cart.filter((item) => item !== productToRemove))
   }
 
+  //FUNCION PARA MOSTRAR EL TOTAL DEL CARRITO
+  const precioToNumero = (precioString => {
+    return parseInt(precioString.slice(1))
+   }) 
+
+  const total = cart.reduce((acc,curr) => acc + precioToNumero(curr.precio), 0)
+
+  //FUNCION PARA MOSTRAR ITEMS DE ALMUERZO Y DESAYUNO EN SUS SECCIONES
  const desayuno = comida.filter (plato => plato.horario === 'Desayuno')
  const almuerzo = comida.filter (plato => plato.horario === 'Almuerzo y cena')
 
@@ -61,10 +47,9 @@ export default function Tables () {
   return (
     <div className="mesas-container">
 
-      <header className='top-mesas'>Mesas</header> 
-
+        {/* <header className='top-mesas'>Mesas</header>  */}
+        
       <section className='desayuno'>
-
       <p className='desayuno-header'>Desayuno</p>
       
          <ul>
@@ -95,21 +80,21 @@ export default function Tables () {
 
       <section className='pedido'>
         <header className='total-header'>Pedido ({cart.length})</header>
+           
+        <p className='nombre-clienta'>Nombre de la clienta: <input className='input-nombre' placeholder=' Escriba nombre aquí'></input></p>
 
         {cart.map((item) => (
-          <div className='pedidos' key={item}>
+          <div className='items-del-pedido' key={item}>
             {item.plato}
             {item.precio}
-            <button onClick={() => removeFromCart(item)}>Borrar</button>
+            <button onClick={() => removeFromCart(item)}> <img className='boton-basurero' src='eliminar' alt='Borrar'></img></button>
           </div>
-
         ))}
 
-        {/* <span>{total}</span> */}
+        <p className='total-pedido'>Total: ${total} </p>
 
-        {/* <button onClick={}> <img className='boton-basurero' src='eliminar' alt='borrar'></img></button> */}
-        
-        {/* <img className='nota-pedido' src='https://64.media.tumblr.com/d188740037e1ad3759e515ef57e7269d/bffd6c491df88dba-47/s1280x1920/e81246329eb99aea36cf4a1163feec5a32167940.pnj'></img> */}
+        <button className='boton-enviar'>Enviar a cocina</button>
+
       </section>
     </div>
   );
